e22d47d8915f737f13519c80c44ba5c957bdc1d7
Deni Riswandi <deniriss95@gmail.com>
2022-08-27 14:16:54 +0700

    Installation tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': '#006563',
+      'secondary': '#E6E7E8',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
